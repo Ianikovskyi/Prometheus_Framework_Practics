b9b6c280f16196b3a62737768e2d72686db4a3f7
Oleksii Ianikovskyi <bronvichock@gmail.com>
2024-05-28 20:01:42 -0700

    Pytest module usage for marks and fixtures

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,2 +1,16 @@
-def check_math():
-    assert 7 * 7 == 49
+import pytest
+
+
+@pytest.mark.change
+def test_remove_name(user):
+    user.name = ''
+    assert user.name == ''
+
+
+@pytest.mark.check
+def test_name(user):
+    assert user.name == "Sergii"
+
+@pytest.mark.check
+def test_second_name(user):
+    assert user.second_name == "Butenko"
